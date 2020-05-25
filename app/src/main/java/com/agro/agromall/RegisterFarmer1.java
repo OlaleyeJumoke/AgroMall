a917504a6dccd0dd8f07ef32b2bcb4d0a477a89a
OlaleyeJumoke <olaleyesilver@gmail.com>
2020-05-25 02:54:13 -1100

    initial commit

---
+++ app/src/main/java/com/agro/agromall/RegisterFarmer1.java
@@ -0,0 +1,13 @@
+package com.agro.agromall;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterFarmer1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_farmer1);
+    }
+}
